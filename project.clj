dd69b7b584facf895f81047d7a3297ecacae445a
Honza Pokorny <me@honza.ca>
2012-10-03 21:52:45 -0300

    Add min lein

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                            [org.clojure/tools.logging "0.2.3"]
                            [noir "1.3.0-beta3"]]
             :jvm-opts ["-Xmx1g"]
+            :min-lein-version "2.0.0"
             :main bible.server)
